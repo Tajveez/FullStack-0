b92668f7e98ed978917628131feb21816cd0328b
Tajveez <tajveez786@gmail.com>
2020-06-06 19:27:58 +0500

    Server setup config

--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@ const app = express();
 app.use(bodyparser.json());
 app.use(cors());
 
+// Handling Production
+if (process.env.NODE_ENV === "production") {
+  //static folder
+  app.use(express.static(__dirname + "/public/"));
+
+  // Handling Single Page Application
+  app.get(/.*/, (req, res) => res.send(__dirname + "/public/index.html"));
+}
+
 // setting API route with Express
 const posts = require("./routes/api/posts");
 app.use("/api/posts", posts);
