4a29f65c00659fcdc88845d4519f01a25912492f
Tajveez <tajveez786@gmail.com>
2020-06-06 19:54:10 +0500

    Code update

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "/public/"));
 
   // Handling Single Page Application
-  app.get(/.*/, (req, res) => res.send(__dirname + "/public/index.html"));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
 
 // setting API route with Express
